54d604a48104752f4c03af087ccea404c2fb2fcf
Shubhomoy Biswas <shubhomoy.tubu@gmail.com>
2020-04-26 12:02:19 +0530

    Updates

---
+++ precache-manifest.ab565ff1acddbd1307f7933baed28372.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2faf30f1715489563cfe0573cf0d69",
+    "url": "/NeoWeatherApp/index.html"
+  },
+  {
+    "revision": "ca1ccb389f0302f107ef",
+    "url": "/NeoWeatherApp/static/css/main.fe41c289.chunk.css"
+  },
+  {
+    "revision": "a05f1b5535f7cbcd40d0",
+    "url": "/NeoWeatherApp/static/js/2.c016eff7.chunk.js"
+  },
+  {
+    "revision": "7bb8316484c25b72cc2ad682be143713",
+    "url": "/NeoWeatherApp/static/js/2.c016eff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1ccb389f0302f107ef",
+    "url": "/NeoWeatherApp/static/js/main.a482e4b5.chunk.js"
+  },
+  {
+    "revision": "b08b1c7a669d10735977",
+    "url": "/NeoWeatherApp/static/js/runtime-main.54fc02c8.js"
+  },
+  {
+    "revision": "d0b012d4bed023c8d8e46ca3b0c2d865",
+    "url": "/NeoWeatherApp/static/media/Cabin-Bold.d0b012d4.ttf"
+  },
+  {
+    "revision": "a2dea8dba63d3a50a3449bdd30e395ee",
+    "url": "/NeoWeatherApp/static/media/Cabin-Medium.a2dea8db.ttf"
+  },
+  {
+    "revision": "c92ed59429995a46c5843f14ccc65b46",
+    "url": "/NeoWeatherApp/static/media/Cabin-SemiBold.c92ed594.ttf"
+  },
+  {
+    "revision": "cfb1e955a2bc8de4cda78aca20d0a996",
+    "url": "/NeoWeatherApp/static/media/cloudy.cfb1e955.svg"
+  },
+  {
+    "revision": "cb7f1d9597e52872b28950c8d14dd7b6",
+    "url": "/NeoWeatherApp/static/media/mist.cb7f1d95.svg"
+  },
+  {
+    "revision": "5ee170bc5e43292f72a181905bdf9d5b",
+    "url": "/NeoWeatherApp/static/media/rainy.5ee170bc.svg"
+  },
+  {
+    "revision": "20df6662a50275d0e84cbb4f10740084",
+    "url": "/NeoWeatherApp/static/media/snow.20df6662.svg"
+  },
+  {
+    "revision": "ef790a4b785f3ef1e86b614e0c35a97e",
+    "url": "/NeoWeatherApp/static/media/sun.ef790a4b.svg"
+  },
+  {
+    "revision": "23e71eddf6c1ac2aedea1c3a485e41fc",
+    "url": "/NeoWeatherApp/static/media/sunny-rainy.23e71edd.svg"
+  },
+  {
+    "revision": "4b969a4c6cc99b09168f1458b5cf3499",
+    "url": "/NeoWeatherApp/static/media/thunder.4b969a4c.svg"
+  }
+]);
